4496bb6ea7b5eb5ddac85bd10d00d9fd05e68c8c
KatWine <kwinegardner@pursuit.org>
2024-01-26 11:38:56 -0500

    completed

--- play2.js
+++ play2.js
@@ -8,5 +8,24 @@ console.log(userData)
  */
 
 function getUserFullName(userArr){
+let userFullName= [];
+
+    for(let i=0; i<userArr.length;i++){
+// used the helper function to title case the string.
+    let firstNameCap = capitalizeName(userArr[i].firstName);
+    let lastNameCap =capitalizeName(userArr[i].lastName);
+    let fullName = `${firstNameCap} ${lastNameCap}`;
+    userFullName.push(fullName);
+    }
+    return userFullName;
     
 }
+
+
+
+//helper function, for cleaner code.
+
+function capitalizeName(str){
+    return str[0].toUpperCase() + str.slice(1).toLowerCase();
+}
+console.log(getUserFullName(userData));
