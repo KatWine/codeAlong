d91322c896dd2361ebe7e20323423d0c7e748620
KatWine <kwinegardner@pursuit.org>
2024-01-26 11:25:38 -0500

    my first repo

---
+++ play2.js
@@ -0,0 +1,12 @@
+const userData = require('./data');
+console.log(userData)
+
+/**
+ * Given users,return an array of each of the users' full name capitalized
+ * @param {Object[]} = users array
+ * @return {Object[]} = new array with all of the users' full name,capitalized
+ */
+
+function getUserFullName(userArr){
+    
+}
